6e257a4f6f4222ebc7e59c8ba3e8e02e47e50a5a
Simon Wiles <simonjwiles@gmail.com>
2020-07-13 15:47:34 -0700

    Add substitution for missing &hearts; entity

--- patent_xml_to_csv.py
+++ patent_xml_to_csv.py
@@ -24,7 +24,7 @@ except ImportError:
         return text
 
 
-def replace_missing_mathml_ents(doc):
+def replace_missing_ents(doc):
     """
     Substitute out some undefined entities that appear in the XML
 
@@ -47,12 +47,44 @@ def replace_missing_mathml_ents(doc):
       The version of mmlextra.ent from here:
         https://github.com/martinklepsch/patalyze/blob/master/resources/parsedir/mmlextra.ent
         seems to have what's required?
+
+    ----
+
+    * hearts
+
+      This entity appears in the detailed description for, e.g., 09489911 (note that this
+      description is not extracted by the parser according to the current version of the
+      config file at config/uspto-grants-0105.yaml).
+
+      In the same context the suit "diamonds" is represented by &diams;, and clubs and
+      hearts are presented by <CUSTOM-CHARACTER> elements which reference external TIFF
+      files... (sigh).  This is despite the face that the appropriate XML entities
+      (&hearts;, &diams;, &clubs;, and &spades;) are all defined in the DTDs and .ent
+      files available, but for some reason &hearts; (and &hearts; alone) is missing from
+      isopub.ent, which is the file actually invoked by the DTD specified in the XML
+      (double sigh).
+
+      In some of the DTD files the symbols specified for the suits are the white glpyhs
+      (i.e. &#x2661, &#x2662, &#x2664, and &#x2667 for hearts, diamonds, spades and clubs
+      respectively), and in others they are the black glyphs (i.e. &#x2665, &#x2666,
+      &#x2660, and &#x2663) -- see, e.g.
+      https://en.wikipedia.org/wiki/List_of_Unicode_characters#Miscellaneous_Symbols.
+
+      I've chose the black variant here, as the black variants are used in isopub.ent
+      -- but note that Google Patents has used the black variants for diamonds
+      (presumably from isopub.ent), the white variant for hearts (coopted from another
+      DTD?), and has dropped the <CUSTOM-CHARACTER> elements for spades and clubs (see
+      https://patents.google.com/patent/US6612926).
+
     """
+
     doc = doc.replace("&IndentingNewLine;", "&#xF3A3;")
     doc = doc.replace("&LeftBracketingBar;", "&#xF603;")
     doc = doc.replace("&RightBracketingBar;", "&#xF604;")
     doc = doc.replace("&LeftDoubleBracketingBar;", "&#xF605;")
     doc = doc.replace("&RightDoubleBracketingBar;", "&#xF606;")
+
+    doc = doc.replace("&hearts;", "&#x2665;")
     return doc
 
 
@@ -274,7 +306,7 @@ class PatentXmlToTabular:
         self.process_field(elems, tree, path, config, record, parent_entity, parent_pk)
 
     def parse_tree(self, doc):
-        doc = replace_missing_mathml_ents(doc)
+        doc = replace_missing_ents(doc)
         return etree.parse(BytesIO(doc.encode("utf8")), self.parser)
 
     def process_doc(self, doc):
