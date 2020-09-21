28e2b18ef895aa33ce7ef45201b76b475fcff1f2
Simon Wiles <simonjwiles@gmail.com>
2020-09-20 17:41:17 -0700

    Split generic classes and helper functions and classes into new package

---
+++ xmltotabular/utils.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+from lxml import etree
+
+
+def expand_paths(path_expr):
+    path = Path(path_expr).expanduser()
+    return Path(path.root).glob(
+        str(Path("").joinpath(*path.parts[1:] if path.is_absolute() else path.parts))
+    )
+
+
+class DTDResolver(etree.Resolver):
+    def __init__(self, dtd_path):
+        self.dtd_path = Path(dtd_path)
+
+    def resolve(self, system_url, _public_id, context):
+        if system_url.startswith(str(self.dtd_path)):
+            return self.resolve_filename(system_url, context)
+        else:
+            return self.resolve_filename(
+                str((self.dtd_path / system_url).resolve()), context
+            )
